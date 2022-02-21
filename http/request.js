e10fe8afbc72dced1e0d43fe2ffe7069ca191740
fanjiale <1345198896@qq.com>
2022-02-21 14:57:30 +0800

    ss

--- http/request.js
+++ http/request.js
@@ -24,10 +24,11 @@
 // 		}
 // =======
 uni.$u.http.setConfig(config => {
-	config.baseURL = 'http://211.149.135.240:7788/front/'
+	config.baseURL = 'http://211.149.135.240:7799/front/'
 	config.timeout = 5000
 	config.header = {
-		'content-type': 'application/x-www-form-urlencoded' || 'application/json'
+		'content-type': 'application/x-www-form-urlencoded' || 'application/json',
+		'token': uni.getStorageSync('token')
 	}
 	return config
 })
