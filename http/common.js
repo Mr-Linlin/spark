60447840abd7e1e1f9c90377ff5401c142a05a3f
fanjiale <1345198896@qq.com>
2022-02-21 14:45:12 +0800

    ss

--- http/common.js
+++ http/common.js
@@ -2,3 +2,6 @@ import http from './request.js'
 
 // 测试登录接口
 export const Login = user => http.post('login', user)
+
+// 资产统计
+export const statistic = user => http.post('/financial/asset/statistic', user)
