d1b138d205d4d2467a225e5601175e7ec10b1995
fanjiale <1345198896@qq.com>
2022-02-28 11:14:52 +0800

    ss

--- pages/user/setUp.vue
+++ pages/user/setUp.vue
@@ -115,7 +115,7 @@
 			</view>
 		</u-popup>
 
-		<!--    交易密码-->
+		<!--    资金密码-->
 		<u-popup :round="20" :show="show2" @close="handleClose" @open="open">
 			<view class="" style="background-color: #F7FAFF;border-radius: 32rpx;">
 				<image @click="handleClose" class="close-img" :src="require('../../static/img/close.png')"></image>
@@ -128,7 +128,7 @@
 							<view>
 								<view class="title p75">验证当前资金密码</view>
 								<view class="code-box p75">
-									<u-code-input size="48" dot class="code-input" v-model="tradePwd" :maxlength="6"
+									<u-code-input size="45" dot class="code-input" v-model="tradePwd" :maxlength="6"
 										borderColor="#fff"></u-code-input>
 								</view>
 								<u-button @click="handleNext()" class="btn-shadow" color="#3A82FE" type="primary"
@@ -139,7 +139,7 @@
 							<view>
 								<view class="title p75">设置新的资金密码</view>
 								<view class="code-box p75">
-									<u-code-input size="48" dot class="code-input" v-model="NewtradePwd" :maxlength="6"
+									<u-code-input size="45" dot class="code-input" v-model="NewtradePwd" :maxlength="6"
 										borderColor="#fff"></u-code-input>
 								</view>
 								<u-button @click="TwotradePwdFun" class="btn-shadow" color="#3A82FE" type="primary"
