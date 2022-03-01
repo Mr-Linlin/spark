7ee0481e13adb24f726a6cea70d077a761312af6
fanjiale <1345198896@qq.com>
2022-03-01 13:49:01 +0800

    ss

--- pages/user/subscribeChild/Scheduling.vue
+++ pages/user/subscribeChild/Scheduling.vue
@@ -5,7 +5,7 @@
 				<text>预排数量</text>
 				<view class="sched_input">
 					<u--input :maxlength="8" v-model="GS" border="none" @change="gsChange" type="number"></u--input>
-					<text style="margin-right: 20rpx;">可用GS {{poolassetData.gs ? poolassetData.gs : 0}}</text>
+					<text style="margin-right: 20rpx;">可用GS {{gsDatava ? gsDatava : 0}}</text>
 				</view>
 			</view>
 			<view class="fnt_num"> 
@@ -14,12 +14,12 @@
 				</view>
 			</view>
 			<view class="group_2">
-				<view style="color:rgba(247, 69, 57, 1) ;padding-top: 24rpx;">FNT可用{{poolassetData.fnt ? poolassetData.fnt : 0}}</view>
+				<view style="color:rgba(247, 69, 57, 1) ;padding-top: 24rpx;">FNT可用{{fntDatava ? fntDatava : 0}}</view>
 				<view style="margin-right: 10rpx;">所需体力FNT=GS*{{getRateData*100}}%*2倍</view><br>
 			</view>
 		</view>
 		<view class="sched-btn">
-			<u-button text="确定预存" class="btn" @click="poolrechargeFun" v-if="FNT > 0 && FNT!==null && FNT < poolassetData.fnt">
+			<u-button text="确定预存" class="btn" @click="poolrechargeFun" v-if="FNT > 0 && FNT!==null && FNT < fntDatava">
 			</u-button>
 			<u-button text="确定预存" class="btn1" @click="poolrechargeFun" :disabled="true" v-else>
 			</u-button>
@@ -41,9 +41,10 @@
 			return {
 				GS: 0,
 				FNT: 0,
-				poolassetData:{},
 				getRateData:'',
-				getPriceData:''
+				getPriceData:'',
+				fntDatava:'',
+				gsDatava:''
 			}
 		},
 		onShow() {
@@ -89,7 +90,7 @@
 					walletType:'1'
 				}
 				getbalance(data).then(res=>{
-					this.poolassetData.fnt = res.obj
+					this.fntDatava = res.obj
 				})
 				
 				let data2 = {
@@ -97,13 +98,13 @@
 					walletType:'1'
 				}
 				getbalance(data2).then(res=>{
-					this.poolassetData.gs = res.obj
+					this.gsDatava = res.obj
 				})
 			},
 			gsChange() {
 				this.FNT = ((this.GS * this.getRateData)*2)/this.getPriceData
-				if (this.FNT > this.poolassetData.fnt) {
-					uni.$u.toast('兑换FNT不能超过'+this.poolassetData.fnt)
+				if (this.FNT > this.fntDatava) {
+					uni.$u.toast('兑换FNT不能超过'+this.fntDatava)
 				} else if (this.FNT > 0 && this.FNT !== null) {
 					this.disabled = false
 				}
